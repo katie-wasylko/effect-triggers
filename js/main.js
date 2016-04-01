71a3c1f37afab5e1082cbe9d459313716b14a76d
katie-wasylko <kwasylko@hotmail.com>
2016-04-01 16:39:46 -0400

    spacing issues

--- js/main.js
+++ js/main.js
@@ -1,26 +1,26 @@
-var $showHide=$ ('.btn-show-hide');
-var $btnMove=$ ('.btn-move');
-var $collExpand=$ ('.btn-collapse-expand');
-var $btnBounce=$ ('.btn-bounce');
-var $btnAppend=$ ('.btn-append');
+var $showHide = $('.btn-show-hide');
+var $btnMove = $('.btn-move');
+var $collExpand = $('.btn-collapse-expand');
+var $btnBounce = $('.btn-bounce');
+var $btnAppend = $('.btn-append');
 
-$showHide.on ('click', function(){
+$showHide.on('click', function(){
   $('.box').toggleClass('show');
 });
 
-$btnMove.on ('click', function(){
+$btnMove.on('click', function(){
   $('.diamond').toggleClass('move');
 });
 
-$collExpand.on ('click', function(){
+$collExpand.on('click', function(){
   $('.panel').toggleClass('collapse');
 });
 
-$btnBounce.on ('click', function(){
+$btnBounce.on('click', function(){
   $('.circle').toggleClass('bounce');
 });
 
-$btnAppend.on ('click', function(){
+$btnAppend.on('click', function(){
   var $li = $('<li>');
 
     $li.html('New List Item');
