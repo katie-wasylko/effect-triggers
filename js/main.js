d24857e90a22a6dd81343e74d8c0d7339afa57ae
katie-wasylko <kwasylko@hotmail.com>
2016-04-01 16:21:47 -0400

    started the js for the other buttons

--- js/main.js
+++ js/main.js
@@ -1,11 +1,25 @@
 var $showHide=$('.btn-show-hide');
 var $btnMove=$('.btn-move');
-
+var $collExpand=$('.btn-collapse-expand');
+var $btnBounce=$('.btn-bounce');
+var $btnAppend=$('.btn-append');
 
 $showHide.on('click', function(){
   $('.box').toggleClass('show');
 });
 
 $btnMove.on('click', function(){
-  $('.box').toggleClass('move');
+  $('.diamond').toggleClass('move');
+});
+
+$btnMove.on('click', function(){
+  $('.panel').toggleClass('move');
+});
+
+$btnMove.on('click', function(){
+  $('.circle').toggleClass('move');
+});
+
+$btnMove.on('click', function(){
+  $('.list').toggleClass('move');
 });
